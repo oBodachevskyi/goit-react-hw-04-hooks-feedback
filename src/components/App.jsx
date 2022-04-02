f119c2ffdd3de71475676ebe67adf3a8b609b5cc
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2022-04-02 18:17:27 +0300

    copmleted tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,54 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Statistics from 'components/Statistics/Statistics'
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions'
 import Section from 'components/Section/Section'
 import Notification from 'components/Notification/Notification';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+
+export default function App () {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+ 
+  const stateArray = {
+    Good: good,
+    Neutral: neutral,
+    Bad: bad,
   }
 
-  onButtonPress = (e) => {  
-    const name = e.target.innerHTML;   
-      this.setState((prevState) => ({
-        [name]: prevState[name] + 1       
-      }))    
-     } 
+  const onButtonPress = (e) => {  
+      switch(e.target.textContent.toLowerCase()) {
+        case 'good':
+          setGood(good + 1)
+          break;
 
-  render () {
-    const stateArray = this.state; 
-   
-    return (      
-     <div> 
-       <Section title="Please leave feedback">
-        
-        <FeedbackOptions 
-          options={Object.keys(stateArray)} 
-          onLeaveFeedback={this.onButtonPress}>            
-          </FeedbackOptions>
-          {(Object.values(stateArray).reduce((acc, num) => acc + num, 0) > 0) ?
-          <Statistics 
-          renderObject={stateArray} /> :
-          <Notification message="There is no feedback" />} 
+          case 'neutral':
+            setNeutral(neutral + 1)
+            break;
+
+            case 'bad':
+              setBad(bad + 1)
+              break;
+
+              default:
+        return;
+      }} 
   
-        </Section>
-      </div>
-    )
-  }
+  return (      
+    <div> 
+      <Section title="Please leave feedback">
+       
+       <FeedbackOptions 
+         options={Object.keys(stateArray)} 
+         onLeaveFeedback={onButtonPress}>            
+         </FeedbackOptions>
+         {(Object.values(stateArray).reduce((acc, num) => acc + num, 0) > 0) ?
+         <Statistics 
+         renderObject={stateArray} /> :
+         <Notification message="There is no feedback" />} 
+ 
+       </Section>
+     </div>
+   )
 }
-
-
-
-export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,35 +1,21 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Statistics extends Component {
-    countTotalFeedback = ({renderObject}) => 
+export default function Statistics (props) {
+
+        const countTotalFeedback = ({renderObject}) =>
         (Object.values(renderObject).reduce((acc, num) => acc + num, 0))
-    
-       
-       countPositiveFeedbackPercentage = ({renderObject}) =>      
-        (Math.round(renderObject['good']/(Object.values(renderObject).reduce((acc, num) => acc + num, 0))*100)) 
 
-/*     countPositiveFeedbackPercentage = () =>      
-      (Math.round(/()*100)) */
+        const countPositiveFeedbackPercentage = ({renderObject}) =>
+        (Math.round(renderObject['Good']/(Object.values(renderObject).reduce((acc, num) => acc + num, 0))*100))
 
-
-    render () {
-      const stateElements = this.props.renderObject     
-        
-      const nameOfStatsticsItems = Object.keys(stateElements)
-        return (<><h2>Statistics</h2>
-        {nameOfStatsticsItems.map((item) =>
-         (<p key={item}>{item}: {stateElements[`${item}`] }</p>)
+    return (<><h2>Statistics</h2>
+        {Object.keys(props.renderObject).map((item) =>
+        (<p key={item}>{item}: {props.renderObject[`${item}`] }</p>)
         )}
-        <p>Total: {this.countTotalFeedback(this.props)}</p>
-        <p>Positive feedback: {this.countPositiveFeedbackPercentage(this.props)}%</p></>)
-    }
+        <p>Total: {countTotalFeedback(props)}</p>
+        <p>Positive feedback: {countPositiveFeedbackPercentage(props)}%</p></>)
 }
 
-Statistics.propTypes = {
+Statistics.propTypes = { 
     renderObject: PropTypes.object,
-    
-} 
-
-export default Statistics
-
+}
